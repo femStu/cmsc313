72c5971e63607c9e4178ef4916e35e2f1b3f2cf5
femStu <a460@umbc.edu>
2025-12-09 01:33:12 +0000

    hw4

---
+++ hw4/hw4.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+ 
+int b2; int b2;
+ 
+void fun(){printf("\nIn fun");}
+ 
+void main() {
+ 
+ printf("this is a test");
+ const int yy = 345;
+ int *ptrA; ptrA = &yy;
+ int new = 10;
+ void *x; int *ptrB = x;
+ void *xx; x = ptrB;
+ const int z; printf("%d", z);
+ char array[5] = "abcdef";
+ fun(10,"GfG", "GBB", 34.5);
+ 
+}
+ 
+int myFunction() { return 0; }
